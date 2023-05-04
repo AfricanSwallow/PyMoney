9b9fbc9eb68c8875e406376512866d1c8d033c6f
AfricanSwallow <shrubbery@gapp.nthu.edu.tw>
2023-05-04 19:52:55 +0800

    week 11

--- pymoney.py
+++ pymoney.py
@@ -1,165 +1,188 @@
 import sys
 
-def initialize():
-    """This open and read from a file that stores records.
-    If cannot open or read the file, then prompt the user for initial money.
-    """
-    records = []
-    try:
-        fh = open('records.txt', 'r')
-        initial_money = int(fh.readline())
-        L = fh.readlines()
-        for item in L:
-            item = item[:-1].split(' ')
-            d = dict(category=item[0], description=item[1], amount=int(item[2]))
-            records.append(d)
-    except:
+class Record:
+    """Represent a record."""
+    def __init__(self, category, description, amount):
+        self._category = category
+        self._descripttion = description
+        self._amount = amount
+
+    @property
+    def category(self):
+        return self._category
+    @property
+    def description(self):
+        return self._descripttion
+    @property
+    def amount(self):
+        return self._amount
+
+class Records:
+    """Maintain a list of all the 'Record's and the initial amount of money."""
+    def __init__(self):
+        self._records = []
         try:
-            initial_money = int(input('How much money do you have? '))
+            fh = open('records.txt', 'r')
+            self._initial_money = int(fh.readline())
+            L = fh.readlines()
+            for item in L:
+                item = item[:-1].split(' ')
+                r = Record(category=item[0], description=item[1], amount=int(item[2]))
+                self._records.append(r)
+        except:
+            try:
+                self._initial_money = int(input('How much money do you have? '))
+            except ValueError:
+                sys.stderr.write('Invalid value for money. Set to 0 by default.\n')
+                self._initial_money = 0
+        else:
+            print('Welcome back!')
+            fh.close()
+
+    def add(self, record, categories):
+        """Add a record"""
+        record = record.split()
+        valid_category = categories.is_category_valid(record[0])
+        if len(record) != 3 or not valid_category:
+            if len(record) != 3:
+                sys.stderr.write('The format of a record should be like this: meal breakfast -50.\n')
+            elif not valid_category:
+                sys.stderr.write('The specified category is not in the category list.\n')
+                sys.stderr.write('You can check the category list by command "view categories".\n')
+            sys.stderr.write('Fail to add a record.\n')
+            return
+        try:
+            r = Record(category=record[0], description=record[1], amount=int(record[2]))
         except ValueError:
-            sys.stderr.write('Invalid value for money. Set to 0 by default.\n')
-            initial_money = 0
-    else:
-        print('Welcome back!')
-        fh.close()
-    return initial_money, records
+            sys.stderr.write('Invalid value for money.\nFail to add a record.\n')
+        else:
+            self._initial_money += r.amount
+            self._records.append(r)
 
-def initialize_categories():
-    """Initialize a categories list"""
-    L = ['expense', ['food', ['meal', 'snack', 'drink'], 'transportation', ['bus', 'railway']], 'income', ['salary', 'bonus']]
-    return L
+    def view(self):
+        """view the current records"""
+        print("Here's your expense and income records:")
+        print(f"   {'Category':<15} {'Description':<20} {'Amount':<6}")
+        print(f"{'='*2} {'='*15} {'='*20} {'='*6}")
+        for i, r in enumerate(self._records):
+            print(f"{i:<2} {r.category:15} {r.description:20} {r.amount:<6}")
+        print(f"{'='*46}")
+        print(f"Now you have {self._initial_money} dollars.")
 
-def is_category_valid(category, categories):
-    """Check if the category name is in the categories list"""
-    if type(categories) == list:
-        for v in categories:
-            b = is_category_valid(category, v)
-            if b == True:
-                return True
-    return categories == category
-   
-def add(initial_money, records, categories):
-    """Add a record"""
-    print('Add an expense or income record with category, description, and amount(separate by spaces):')
-    item = input().split(' ')
-    valid_category = is_category_valid(item[0], categories)
-    if len(item) != 3 or not valid_category:
-        if len(item) != 3:
-            sys.stderr.write('The format of a record should be like this: meal breakfast -50.\n')
-        elif not valid_category:
+    def delete(self, delete_record):
+        """delete a record from records"""
+        try:
+            i = int(delete_record)
+            self._initial_money -= self._records[i].amount
+        except IndexError:
+            sys.stderr.write(f'Index out of range. There if no line {i}.\n')
+        except ValueError:
+            sys.stderr.write('Invalid format. Fail to delete a record.\n')
+        else:
+            print(f'delete {self._records.pop(i)}')
+
+    def find(self, target_categories, category):
+        """Prompt for a category name to find. Find all the records that belongs to that category.
+        Print out the filtered records and report the total amount of money of the listed records."""
+        if target_categories == []:
             sys.stderr.write('The specified category is not in the category list.\n')
             sys.stderr.write('You can check the category list by command "view categories".\n')
-        sys.stderr.write('Fail to add a record.\n')
-        return initial_money, records
-    try:
-        d = dict(category=item[0], description=item[1], amount=int(item[2]))
-    except ValueError:
-        sys.stderr.write('Invalid value for money.\nFail to add a record.\n')
-    else:
-        initial_money += d['amount']
-        records.append(d)
-    return initial_money, records
+            return
+        filtered_records = list(filter((lambda r: r.category in target_categories), self._records))
+        print(f'Here\'s your expense and income records under category "{category}":')
+        print(f"{'Category':<15} {'Description':<20} {'Amount':<6}")
+        print(f"{'='*15} {'='*20} {'='*6}")
+        total_amount = 0
+        for r in filtered_records:
+            print(f"{r.category:15} {r.description:20} {r.amount:<6}")
+            total_amount += r.amount
+        print(f"{'='*43}")
+        print(f"The total amount above is {total_amount}.")
 
-def view(initial_money, records):
-    """view the current records"""
-    print("Here's your expense and income records:")
-    print(f"   {'Category':<15} {'Description':<20} {'Amount':<6}")
-    print(f"{'='*2} {'='*15} {'='*20} {'='*6}")
-    for i, d in enumerate(records):
-        print(f"{i:<2} {d['category']:15} {d['description']:20} {d['amount']:<6}")
-    print(f"{'='*46}")
-    print(f"Now you have {initial_money} dollars.")
+    def save(self):
+        """Save the records to a file"""
+        with open('records.txt', 'w') as fh:
+            fh.write(f'{self._initial_money}\n')
+            for r in self._records:
+                fh.write(f"{r.category} {r.description} {r.amount}\n")
 
-def delete(initial_money, records):
-    """delete a record from records"""
-    try:
-        i = int(input('Which line of record do you want to delete? '))
-        initial_money -= records[i]['amount']
-    except IndexError:
-        sys.stderr.write(f'Index out of range. There if no line {i}.\n')
-    except ValueError:
-        sys.stderr.write('Invalid format. Fail to delete a record.\n')
-    else:
-        print(f'delete {records.pop(i)}')
-    return initial_money, records
+class Categories:
+    """Maintain the category list and provide some methods."""
+    def __init__(self):
+        self._categories = ['expense', ['food', ['meal', 'snack', 'drink'], 'transportation', ['bus', 'railway']], 'income', ['salary', 'bonus']]
+    
+    def view(self):
+        """view all the categories"""
+        def view_categories(categories = self._categories, level = -1):
+            if type(categories) == list:
+                for child in categories:
+                    view_categories(child, level+1)
+            else:
+                print(f'{" "*2*level}- {categories}')
+        view_categories()
+        
+    
+    def is_category_valid(self, category):
+        """Check if the category name is in the categories list"""
+        def is_valid(category, categories):
+            if type(categories) == list:
+                for v in categories:
+                    b = is_valid(category, v)
+                    if b == True:
+                        return True
+            return categories == category
+        return is_valid(category, self._categories)
 
-def view_categories(categories, level = -1):
-    """view all the categories"""
-    if type(categories) == list:
-        for child in categories:
-            view_categories(child, level+1)
-    else:
-        print(f'{" "*2*level}- {categories}')
-
-def find_subcategories(category, categories):
-    """Take a category name and return a non-nested list containing 
-    the specified category and all the subcategories"""
-    if type(categories) == list:
-        for i, v in enumerate(categories):
-            p = find_subcategories(category, v)
-            if p == True:
-                if (i + 1) < len(categories) and type(categories[i+1]) == list:
-                    return flatten(categories[i:i+2])
-                else:
-                    return [v]
-            if p != []:
-                return p
-    return  True if categories == category else []
-
-def flatten(L):
-    """Take a nested list and return a non-nested list"""
-    if type(L) == list:
-        result = []
-        for child in L:
-            result.extend(flatten(child))
-        return result
-    return [L]
-
-def find(categories, records):
-    """Prompt for a category name to find. Find all the records that belongs to that category.
-    Print out the filtered records and report the total amount of money of the listed records."""
-    category = input('Which category do you want to find? ')
-    found_subcategories = find_subcategories(category, categories)
-    if found_subcategories == []:
-        sys.stderr.write('The specified category is not in the category list.\n')
-        sys.stderr.write('You can check the category list by command "view categories".\n')
-        return
-    filtered_records = list(filter((lambda d: d['category'] in found_subcategories), records))
-    print(f'Here\'s your expense and income records under category "{category}":')
-    print(f"{'Category':<15} {'Description':<20} {'Amount':<6}")
-    print(f"{'='*15} {'='*20} {'='*6}")
-    total_amount = 0
-    for d in filtered_records:
-        print(f"{d['category']:15} {d['description']:20} {d['amount']:<6}")
-        total_amount += d['amount']
-    print(f"{'='*43}")
-    print(f"The total amount above is {total_amount}.")
-
-def save(initial_money, records):
-    """Save the records to a file"""
-    with open('records.txt', 'w') as fh:
-        fh.write(f'{initial_money}\n')
-        for item in records:
-            fh.write(f"{item['category']} {item['description']} {item['amount']}\n")
+    def find_subcategories(self, category):
+        """Take a category name and return a non-nested list containing 
+            the specified category and all the subcategories"""
+        def find(category, categories):
+            if type(categories) == list:
+                for i, v in enumerate(categories):
+                    p = find(category, v)
+                    if p == True:
+                        if (i + 1) < len(categories) and type(categories[i+1]) == list:
+                            return self._flatten(categories[i:i+2])
+                        else:
+                            return [v]
+                    if p != []:
+                        return p
+            return  True if categories == category else []
+        return find(category, self._categories)
+    
+    def _flatten(self, L):
+        """Take a nested list and return a non-nested list"""
+        def flatten(L):
+            if type(L) == list:
+                result = []
+                for child in L:
+                    result.extend(flatten(child))
+                return result
+            return [L]
+        return flatten(L)
 
 
-initial_money, records = initialize()
-categories = initialize_categories()
+categories = Categories()
+records = Records()
 
 while True:
-    cmd = input('\nWhat do you want to do (add / view / delete / view categories / find / exit)? ')
-    if cmd == 'add':
-        initial_money, records = add(initial_money, records, categories)
-    elif cmd == 'view':
-        view(initial_money, records)
-    elif cmd == 'delete':
-        initial_money, records = delete(initial_money, records)
-    elif cmd == 'view categories':
-        view_categories(categories)
-    elif cmd == 'find':
-        find(categories, records)
-    elif cmd == 'exit':
-        save(initial_money, records)
+    command = input('\nWhat do you want to do (add / view / delete / view categories / find / exit)? ')
+    if command == 'add':
+        record = input('Add an expense or income record with category, description, and amount(separate by spaces):\n')
+        records.add(record, categories)        
+    elif command == 'view':
+        records.view()
+    elif command == 'delete':
+        delete_record = input('Which line of record do you want to delete? ')
+        records.delete(delete_record)
+    elif command == 'view categories':
+        categories.view()
+    elif command == 'find':
+        category = input('Which category do you want to find? ')
+        target_categories = categories.find_subcategories(category)
+        records.find(target_categories, category)
+    elif command == 'exit':
+        records.save()
         break
     else:
         sys.stderr.write('Invalid command. Try again.\n')
